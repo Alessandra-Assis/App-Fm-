afd1d070947dbd0b671b97376e0c40f71971909a
Alessandra-Assis <alessandra.assis@shopee.com>
2026-04-15 22:50:26 -0300

    Update get.js

--- api/get.js
+++ api/get.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   try {
-    const response = await fetch('SUA_URL_DO_APPS_SCRIPT?action=get');
+    const response = await fetch('https://script.google.com/a/macros/shopee.com/s/AKfycbxpYpULOOj9yqab2zuCSxmPhEhupmoKBWZfFNGHelMpIrU-mrdvrJSkum5_oAAN97O6/exec?action=get');
     const data = await response.json();
 
     res.status(200).json(data);
